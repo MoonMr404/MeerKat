0fec6485d2881e4967ea01e49f685627b17dcb08
Stefano Nicolò Zito <162315477+stefzito8@users.noreply.github.com>
2025-02-07 16:16:40 +0100

    Implemented Main Window Sidebar
    First implementation for the main window's sidebar able of moving between pages and collapsing.

--- ClientAvalonia/ViewModels/MainWindowViewModel.cs
+++ ClientAvalonia/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,55 @@
-﻿namespace ClientAvalonia.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ClientAvalonia.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+     [ObservableProperty]
+     private bool _IsPaneOpen = true;
+
+     [ObservableProperty] private ViewModelBase _currentPage = new UserViewModel();
+
+     [ObservableProperty] private PageListTemplate? _selectedListItem;
+
+     public ObservableCollection<PageListTemplate> Pages { get; } = new()
+     {
+          new PageListTemplate(typeof(UserViewModel), "personregular"),
+          new PageListTemplate(typeof(TeamsViewModel), "peoplecommunityregular")
+     };
+     
+     partial void OnSelectedListItemChanged(PageListTemplate? value)
+     {
+          if (value is null) return;
+          var instance= Activator.CreateInstance(value.ModelType);
+          if (instance is null) return;
+          CurrentPage = (ViewModelBase)instance;
+     }
+     
+     [RelayCommand]
+     private void TriggerPane()
+     {
+          IsPaneOpen = !IsPaneOpen;
+     }
+}
+
+public class PageListTemplate
+{
+     public PageListTemplate(Type type, String iconKey)
+     {
+          ModelType =type;
+          Label= type.Name.Replace("ViewModel", "");
+
+          Application.Current!.TryFindResource(iconKey, out var res);
+          ListItemIcon = (StreamGeometry)res!;
+     }
+
+     public String Label { get; }
+     public Type ModelType { get; }
+     public StreamGeometry ListItemIcon { get; }
 }
---
+++ ClientAvalonia/Views/UserView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ClientAvalonia.Views;
+
+public partial class UserView : UserControl
+{
+    public UserView()
+    {
+        InitializeComponent();
+    }
+}
