e7b7c61dee0dd01c0741454ec91a39ff56e2ebf2
Stefano Nicolò Zito <162315477+stefzito8@users.noreply.github.com>
2025-02-10 11:37:38 +0100

    Revert "Setup del Database e API di base"

--- ClientAvalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ClientAvalonia.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- ClientAvalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ClientAvalonia.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
