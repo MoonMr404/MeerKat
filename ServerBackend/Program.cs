d90381eb7b2bfbf91edb09cb440917fb8a25362f
Gabriel Tabasco <41172833+Greenylie@users.noreply.github.com>
2025-02-10 12:15:43 +0100

    Rework of the database structure: Models in Backend DTOs are shared Added controllers

--- ServerBackend/Program.cs
+++ ServerBackend/Program.cs
@@ -1,41 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ServerBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var config = builder.Configuration;
+
+if (config["REAL_DATABASE"] == null || config["REAL_DATABASE"] == "false")
+{
+    //Memory database per il testing
+    builder.Services.AddDbContext<MeerkatContext>(options =>
+        options.UseInMemoryDatabase("meerkat"));
+}
+else
+{
+    //Database SQL, controllare appsettings.Development.json per l'acesso
+    var connectionString = builder.Configuration.GetConnectionString("Local");
+    builder.Services.AddDbContext<MeerkatContext>(options =>
+         options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))); 
+}
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
