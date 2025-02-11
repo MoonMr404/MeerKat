f325d9fe7e4e4720e1b7606814a26067110d378e
Gabriel Tabasco <41172833+Greenylie@users.noreply.github.com>
2025-02-11 12:14:51 +0100

    Frontend registration prompts the API with an HTTPRequest

---
+++ ClientAvalonia/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientAvalonia.Services;
+
+using System.Net.Http.Json;
+using Shared.Dto;
+
+public class UserService(HttpClient httpClient, string apiBaseUrl)
+{
+    private readonly string _baseUrl = $"{apiBaseUrl}/api/user";
+
+    
+    public async Task<IEnumerable<UserDto>> GetUsersAsync(bool nested = false)
+    {
+        var users = await httpClient.GetFromJsonAsync<IEnumerable<UserDto>>($"{_baseUrl}?nested={nested}");
+        return users ?? Enumerable.Empty<UserDto>();
+    }
+
+    public async Task<UserDto?> GetUserByIdAsync(Guid id, bool nested = false)
+    {
+        try
+        {
+            return await httpClient.GetFromJsonAsync<UserDto>($"{_baseUrl}/{id}?nested={nested}");
+        }
+        catch (HttpRequestException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+    }
+
+    public async Task<UserDto> CreateUserAsync(UserDto user)
+    {
+        var response = await httpClient.PostAsJsonAsync(_baseUrl, user);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<UserDto>() 
+               ?? throw new InvalidOperationException("Failed to deserialize the created user");
+    }
+
+    public async Task<UserDto> UpdateUserAsync(UserDto user)
+    {
+        var response = await httpClient.PutAsJsonAsync(_baseUrl, user);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<UserDto>() 
+               ?? throw new InvalidOperationException("Failed to deserialize the updated user");
+    }
+
+    public async Task DeleteUserAsync(Guid id)
+    {
+        var response = await httpClient.DeleteAsync($"{_baseUrl}/{id}");
+        response.EnsureSuccessStatusCode();
+    }
+}
--- ClientAvalonia/ViewModels/RegistrationViewModel.cs
+++ ClientAvalonia/ViewModels/RegistrationViewModel.cs
@@ -1,15 +1,50 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using ClientAvalonia.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Shared.Dto;
+using Splat;
 
 namespace ClientAvalonia.ViewModels;
 
 public partial class RegistrationViewModel : ViewModelBase
 {
     [ObservableProperty] private bool _passwordVisible;
+    
+    //DTO
+    [ObservableProperty] private string _name;
+    [ObservableProperty] private string _surname;
+    [ObservableProperty] private string _email;
+    [ObservableProperty] private string _password;
+    [ObservableProperty] private string _passwordVerify;
+    [ObservableProperty] private DateTime _dateOfBirth = DateTime.Today;
+    
+    private UserService userService;
+    
+    
+    public RegistrationViewModel()
+    {
+        userService = Locator.Current.GetService<UserService>() ?? throw new InvalidOperationException();
+    }
    
     [RelayCommand]
     private void ChangePasswordVisibility()
     {
         PasswordVisible = !PasswordVisible;
+    }
+
+    [RelayCommand]
+    private async void RegisterUser()
+    {
+        UserDto newUser = new UserDto
+        {
+            Name = _name,
+            Surname = _surname,
+            Email = _email,
+            Password = _password,
+            DateOfBirth = DateOnly.FromDateTime(_dateOfBirth),
+        };
+        
+        Console.WriteLine(await userService.CreateUserAsync(newUser));
     }
 }
